1d666595b76cc4658652e1a0207dbb73fa68cc75
anilhimam17 <anilhimamg@gmail.com>
2026-07-08 23:01:59 +0100

    feat: added utility function load_circuit_config to loaded json circuit files

--- src/mark_1/utils.py
+++ src/mark_1/utils.py
@@ -1,6 +1,12 @@
+# FastF1 Deps
+from fastf1.core import Session
+
+# Data Deps
 from dataclasses import dataclass
 
-from fastf1.core import Session
+# Core Deps
+from json import load
+from importlib import resources
 
 
 @dataclass
@@ -15,3 +21,20 @@ class EventSessions:
     # Sprint Grandprix Weekend
     sprint_quali_session: Session | None = None
     sprint_session: Session | None = None
+
+
+# ============ Utility Functions ============
+
+def load_circuit_config(circuit: str) -> dict[str, str]:
+        """Helper method to load the config file from data."""
+
+        # Accessing the Data Path
+        data_dir = resources.files("mark_1") / "data"
+        json_transversable = data_dir / f"{circuit}.json"
+
+        try:
+            with resources.as_file(json_transversable) as file_path:
+                with open(file_path, encoding="utf-8") as file:
+                    return load(file)
+        except Exception as e:
+            raise AssertionError(f"Error incurred when loading the circuit specification. Failed with {str(e)}")
